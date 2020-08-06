b1b445186d22ea023d725eddb213e6217d04fd7c
Luis Saiete <luistsaiete@gmail.com>
2020-08-06 21:37:18 +0200

    Global styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import './assets/styles/global.css'
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>Hello Universe</h1>
+			<button type="button">
+				Click me
+			</button>
     </div>
   );
 }
