4ae06bfe84b5ec86e62220a1a11f49d1bfe1e888
Luis Saiete <luistsaiete@gmail.com>
2020-08-06 23:21:58 +0200

    Teacher List header

---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './styles.css';
+
+import logoImg from '../../assets/images/logo.svg';
+import backIcon from '../../assets/images/icons/back.svg';
+
+
+function TeacherList() {
+	return (
+		<div id="page-teacher-list" className="container">
+			<header className="page-header" >
+
+				<div className="top-bar-container">
+					<Link to="/" >
+						<img src={backIcon} alt="Voltar"/>
+					</Link>
+
+					<img src={logoImg} alt="Proffy"/>
+				</div>
+
+				<div className="header-content" >
+					<strong>Estes são os Proffys disponíveis.</strong>
+				</div>
+
+			</header>
+		</div>
+	);
+}
+
+export default TeacherList;
