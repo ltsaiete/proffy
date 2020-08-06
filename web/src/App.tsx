fb6f41eb8720db84d680caca17150fca5b5f3d5e
Luis Saiete <luistsaiete@gmail.com>
2020-08-06 22:02:13 +0200

    Structuring landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 
+import Landing from './pages/Landing'
+
 import './assets/styles/global.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Universe</h1>
-			<button type="button">
-				Click me
-			</button>
-    </div>
+    <Landing />
   );
 }
 
