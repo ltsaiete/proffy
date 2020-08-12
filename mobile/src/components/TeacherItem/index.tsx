8b1e0c33f30fe42b0fb84f7512f4768e6f1d16b3
Luis Saiete <luistsaiete@gmail.com>
2020-08-12 23:40:53 +0200

    Contact via whatsapp

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, Linking } from 'react-native';
 
 import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
 import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
@@ -26,6 +26,10 @@ interface TeacherItemProps {
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+	function handleLinkToWhatsapp(){
+		Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`)
+	}
+
 	return (
 		<View style={styles.container}>
 			<View style={styles.profile}>
@@ -57,7 +61,7 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
 						<Image source={unfavoriteIcon} />
 					</RectButton>
 
-					<RectButton style={styles.contactButton}>
+					<RectButton style={styles.contactButton} onPress={handleLinkToWhatsapp}>
 						<Image source={whatsappIcon} />
 						<Text style={styles.contactButtonText}>Entrar em contacto</Text>
 					</RectButton>
