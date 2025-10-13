7c86d1ceba3e4c1382a90a1ffa9576eb176bc008
Luis Saiete <luistsaiete@gmail.com>
2025-10-13 16:33:34 +0200

    Setup factories

--- server/src/http/controllers/register.ts
+++ server/src/http/controllers/register.ts
@@ -1,8 +1,7 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import z from 'zod'
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
-import { RegisterUseCase } from '@/use-cases/register'
+import { makeRegisterUseCase } from '@/use-cases/factories/make-register-use-case'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -15,10 +14,8 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { email, latitude, longitude, name, password } =
     registerBodySchema.parse(request.body)
 
-  const usersRepository = new PrismaUsersRepository()
-  const registerUseCase = new RegisterUseCase(usersRepository)
-
   try {
+    const registerUseCase = makeRegisterUseCase()
     await registerUseCase.execute({
       email,
       latitude,
